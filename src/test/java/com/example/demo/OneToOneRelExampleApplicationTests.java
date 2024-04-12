5d50fbaf4ab14a1933b5e27890be365b23277d4d
MuhammadFaizan980 <mohammadfaizan980@gmail.com>
2024-04-12 05:34:47 +0300

    frat: added unit test for toAnnualReportTest()

--- src/test/java/com/example/demo/OneToOneRelExampleApplicationTests.java
+++ src/test/java/com/example/demo/OneToOneRelExampleApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.example.demo.dtos.annual_report.AnnualReportRequest;
+import com.example.demo.entities.AnnualReport;
+import com.example.demo.mappers.AnnualReportMapper;
+import org.junit.jupiter.api.*;
 
-@SpringBootTest
 class OneToOneRelExampleApplicationTests {
+    private AnnualReportMapper annualReportMapper;
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void beforeAll() {
+        // called just once before execution of tests begins
+    }
 
+    @AfterAll
+    static void afterAll() {
+        // called just once after all tests are completed
+    }
+
+    @BeforeEach
+    void setUp() {
+        // called before each test
+        annualReportMapper = new AnnualReportMapper();
+    }
+
+    @AfterEach
+    void tearDown() {
+        // called at the end of each test
+    }
+
+    @Test
+    public void toAnnualReportTest() {
+        AnnualReportRequest annualReportRequest = new AnnualReportRequest("a", 3.21);
+        AnnualReport annualReport = annualReportMapper.toAnnualReport(annualReportRequest);
+        Assertions.assertEquals(annualReport.getStudentGrade(), annualReportRequest.studentGrade());
+        Assertions.assertEquals(annualReport.getStudentGpa(), annualReportRequest.studentGpa());
+    }
 }
